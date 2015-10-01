9051c75b9dbd3d103c0f93dda8f56430b3130520
andbr472 <andrea.bruzzone92@gmail.com>
2015-10-01 16:57:26 +0200

    Update brute_force_knapsack.R

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -1,3 +1,4 @@
+
 #' Brute force solver of Knapsack problem
 #' 
 #' This function solves the Knapsack probem by brute force.
@@ -22,41 +23,13 @@
 #'  @references \url{http://en.wikipedia.org/wiki/Knapsack_problem}  
 #'
 
-getvalues <-function(rowid){
-  index_i <- 1:(2^length(x[,1])-1)
-  index_j <- 1:length(intToBits(1))
-  iandj <- expand.grid(i = index_i, j =index_j)
-  binary <- intToBits()
-  weightsum <- 0
-  valuesum <- 0
-  if(iandj$j[rowid] == TRUE)
-  chosen <- c()
-  matr <- matrix(4,4,4)
-  if( matr[i,j] > 0){
-    weightsum <- weightsum + matr[i,j]
-  }
-  
-  return(weightsum)
-}
 
-brute_force_knapsack <- function(x,W,parallel= FALSE){
+brute_force_knapsack <- function(x,W){
   stopifnot(is.data.frame(x) & x > 0 & names(x) == c("w","v"))
   
   winnervalue <- 0
   winnerpackets <-c()
-  if(parallel){
-    index_i <- 1:(2^length(x[,1])-1)
-    binary <- parallel::mclapply(index_i, intToBits,  mc.cores = 8)
-    index_j <- 1:length(binary[[1]])
-    
-    weightsum <- parallel::mclapply(binary[[index_i]][index_j],insideweights ,mc.cores = 8)
-    valuesum <- parallel::mclapply(binary[[index_i]][index_j], insidevalues ,mc.cores = 8)
-#     print(weightsum)
-#     print(valuesum)
-    
-    return("hi")
-  }
-  for(i in 1:(2^length(x[,1])-1)){
+  for(i in 1:2^length(x[,1])-1){
     weightsum <- 0
     valuesum <- 0
     packetschosen <-c()
@@ -78,5 +51,3 @@ brute_force_knapsack <- function(x,W,parallel= FALSE){
   
   return(list(value=round(winnervalue),elements=winnerpackets))
 }
-
-
