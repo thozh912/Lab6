c8cfc74458b7fca3c61986d37ee2cd8bfd1bd908
andbr472 <andrea.bruzzone92@gmail.com>
2015-10-03 20:02:30 +0200

    a lot

--- R/knapsack_gener_matr.R
+++ R/knapsack_gener_matr.R
@@ -1,3 +1,15 @@
+#' Generates values for knapsack problem
+#' 
+#' This function is the knapsack value generator in the Lab instructions 
+#' for Lab 6 in 732A50. It is the same as the \code{knapsack_generator} function but this one uses 
+#' matrix instead of data frame.
+#' 
+#'
+#' 
+#' @return \code{knapsack_objects} A dataframe with two columns, w and v
+#' containing weights and values for use in the knapsack problem.
+#'
+
 knapsack_gener_matr<-function(){
   set.seed(42)
   n <- 1000000
--- vignettes/lab_report_knapsack.R
+++ vignettes/lab_report_knapsack.R
@@ -1,20 +1,38 @@
-## ------------------------------------------------------------------------
-#brute_force_knapsack(data.frame(w=c(29,11,4,50,33,9,20,6 ),v=c(2,7,11,2,9,33,6,45 )),W =20)
-
-## ---- echo = FALSE-------------------------------------------------------
-#Sys.time()
-#brute_force_knapsack(data.frame(w=c(29,11,4,50,33,9,20,6 ),v=c(2,7,11,2,9,33,6,45 )),W =20)
-#Sys.time()
+## ---- echo = FALSE, message = FALSE--------------------------------------
+library(Lab6)
 
 ## ------------------------------------------------------------------------
 
+#system.time(brute_force_knapsack(knapsack_generator()[1:16,],W =2000))
+
+
+## ------------------------------------------------------------------------
+#system.time(knapsack_dynamic(knapsack_generator()[1:500,],W =2000))
+
+## ------------------------------------------------------------------------
+#system.time(greedy_knapsack(knapsack_generator()[1:1000000,],W =2000))
+
+## ----echo=FALSE----------------------------------------------------------
+
+knapsack_matr <- knapsack_gener_matr()
+knapsack_df <- knapsack_generator()
 
 ## ------------------------------------------------------------------------
 
 
-## ---- echo=FALSE---------------------------------------------------------
+tmp <- tempfile()
+Rprof(tmp,interval = 0.001)
+brute_force_knapsack(knapsack_matr[1:15,], W = 2000)
+Rprof(NULL)
+summaryRprof(tmp)
 
 
-## ---- echo=FALSE---------------------------------------------------------
+tmp <- tempfile()
+Rprof(tmp,interval = 0.001)
+brute_force_knapsack(knapsack_df[1:15,], W = 2000)
+Rprof(NULL)
+summaryRprof(tmp)
 
+## ------------------------------------------------------------------------
+#system.time(brute_force_knapsack(knapsack_generator()[1:16,],W =2000), parallel = TRUE)
 
