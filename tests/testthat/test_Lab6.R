33d76cebc8c506cd420a4b16797fd99a50afd5e6
thozh912 <thomaszhangswe@gmail.com>
2015-10-12 17:38:17 +0200

    rrraaagh

--- tests/testthat/test_Lab6.R
+++ tests/testthat/test_Lab6.R
@@ -13,8 +13,8 @@ test_that("The output is correct ",{
 })
 
 test_that("The elements are correct ",{
-  #expect_equal(knapsack_dynamic(knapsack_generator()[1:20,],3500)$elements,c(18,14,8,3),tolerance =1)
-  #expect_equal(greedy_knapsack(knapsack_generator()[1:20,],3500)$elements, c(8,18,14,3),tolerance = 1)
+  expect_equal(knapsack_dynamic(knapsack_generator()[1:20,],3500)$elements,c(18,14,8,3),tolerance =1e-2)
+  expect_equal(greedy_knapsack(knapsack_generator()[1:20,],3500)$elements, c(8,18,14,3),tolerance = 1)
   expect_equal(brute_force_knapsack(knapsack_generator()[1:7,],10000)$elements, c(1, 3, 5, 6),tolerance =1)           
   
 })
